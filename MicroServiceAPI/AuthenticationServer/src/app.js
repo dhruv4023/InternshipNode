9f082c78ee6244f0ab1e5dae1fcaeef4b82b9327
dhruv <dhruv20345@gmail.com>
2024-01-29 19:01:58 +0530

    cloudnary import

--- MicroServiceAPI/AuthenticationServer/src/app.js
+++ MicroServiceAPI/AuthenticationServer/src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import x from "./cloudnary/cloudnaryConfig.js"
+import "./cloudnary/cloudnaryConfig.js"
 import config from './config/config.js';
 
 const app = express();
@@ -12,7 +12,7 @@ app.use(express.json()); // Parse JSON request bodies
 app.use(helmet()); // Enhance security by setting various HTTP headers
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); // Configure CORS policy
 app.use(morgan("common")); // Log HTTP requests
-app.use(cors({ origin: JSON.parse(config.origin_url_list)})); // Configure CORS for allowed origins
+app.use(cors({ origin: JSON.parse(config.origin_url_list) })); // Configure CORS for allowed origins
 app.get("/", (req, res) => {
   res.send("Server is running...");
 });
